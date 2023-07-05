5e397115402df00fc76aebc96171e260305c20d3
Darren Chan <dfchan@idsdatanet.com>
2023-07-05 19:55:25 +0800

    added in graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::error::Error;
 
+use axum::Router;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello World!");
+    let app = Router::new().route("/", axum::routing::get(|| async { "Hello World" }));
+    let server =
+        axum::Server::bind(&"127.0.0.1:3000".parse().unwrap()).serve(app.into_make_service());
+    server
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
     Ok(())
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+    println!("Gracefully shutting down~");
 }
