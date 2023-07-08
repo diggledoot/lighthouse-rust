1115e14fe167d145a9043a334de15af017e986d2
Darren Chan <dfchan@idsdatanet.com>
2023-07-08 19:34:00 +0800

    remove redundant comments

--- src/post/post_handler.rs
+++ src/post/post_handler.rs
@@ -1,16 +1,14 @@
 use axum::{extract::Path, Json};
 use orm::entities::post;
 
-//c
 pub async fn create_post(Json(payload): Json<post::Model>) -> String {
     format!("create post with the following json {:?}", payload)
 }
-//r
+
 pub async fn get_post_by_id(Path(id): Path<u32>) -> String {
     format!("get on post with an id of {}", id)
 }
 
-//u
 pub async fn update_post_by_id(Path(id): Path<u32>, Json(payload): Json<post::Model>) -> String {
     format!(
         "update post with the following json {:?} of id {}",
@@ -18,7 +16,6 @@ pub async fn update_post_by_id(Path(id): Path<u32>, Json(payload): Json<post::Mo
     )
 }
 
-//d
 pub async fn delete_post_by_id(Path(id): Path<u32>) -> String {
     format!("delete post with an id of {}", id)
 }
