de99859425effa5f2443b1c253cf2a762e15843f
Darren Chan <dfchan@idsdatanet.com>
2023-07-23 08:43:51 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 okay able to pass the db state around the handlers ughhh.
 okay to do create post function. just interacting with the db.
+
+
+no lies. i'll probably be scraping this as circumstances have changed.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod post;
 pub mod utilities;
 
+use std::error::Error;
+
 use axum::{routing::{IntoMakeService, get}, Router};
 use hyper::{StatusCode, Uri, Server, server::conn::AddrIncoming};
 
@@ -15,6 +17,6 @@ pub async fn shutdown_signal() {
     println!("Gracefully shutting down~");
 }
 
-pub fn run()->Server<AddrIncoming,IntoMakeService<Router>>{
+pub fn run()->Result<Server<AddrIncoming,IntoMakeService<Router>>,Box<dyn Error>>{
     todo!("Move the run logic here for slim main")
 }
